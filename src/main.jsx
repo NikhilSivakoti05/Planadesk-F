863c705023edcc2eee38152bbd36d5cede569de5
NS <2300030644@kluniversity.in>
2026-02-27 00:47:30 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.withCredentials = true; // ✅ GLOBAL FIX
 
 createRoot(document.getElementById("root")).render(<App />);
